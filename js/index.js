e26e1c6801675fbe1e200fb64522419829d6513a
Erika Imperial <imperial.erikajane@gmail.com>
2020-04-30 19:17:48 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,19 @@
+//jshint esversion:9
+
+$(document).ready(function() {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true
+      },
+      600: {
+        items: 3,
+        nav: true
+      }
+    }
+  });
+});
